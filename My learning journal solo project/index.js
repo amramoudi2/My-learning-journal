91ddc5870f93f1458a25a74262cf3be02f422cdc
amramoudi2 <amramoudi45@outlook.com>
2024-06-19 17:00:06 +0300

    Delete My learning journal solo project directory

--- My learning journal solo project/index.js
+++
@@ -1,9 +0,0 @@
-const viewMoreBtn = document.getElementById("view-more");
-const moreBlogs = document.getElementsByClassName("b");
-
-viewMoreBtn.addEventListener("click", () => {
-    Array.from(moreBlogs).forEach(blog => {
-        blog.style.display = 'block';
-    });
-    viewMoreBtn.style.display = 'none';
-});
